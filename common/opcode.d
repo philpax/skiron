76610e4ff97e0cb1a1fd7ea9681c7159afa4f1a0
Philpax <me@philpax.me>
2015-09-10 12:15:38 +1000

    Modify opcode format; add loadli and loadui opcodes

--- common/opcode.d
+++ common/opcode.d
@@ -8,16 +8,15 @@ struct Opcode
 	{
 		mixin(bitfields!(
 			ubyte, "opcode", 8,
-			ubyte, "x", 3,
 			ubyte, "register1", 7,
 			ubyte, "register2", 7,
-			ubyte, "register3", 7));
+			ubyte, "register3", 7,
+			ubyte, "x", 3));
 
 		mixin(bitfields!(
 			ubyte, "", 8,
-			ubyte, "", 3,
 			ubyte, "", 7,
-			ubyte, "immediate", 14));
+			ubyte, "immediate", 17));
 	}
 }
 
@@ -26,7 +25,7 @@ static assert(Opcode.sizeof == uint.sizeof);
 enum Encoding
 {
 	A, // dst, src1, src2
-	B  // dst, imm14
+	B  // dst, imm17
 }
 
 struct OpcodeDescriptor
@@ -38,18 +37,20 @@ struct OpcodeDescriptor
 
 enum Opcodes
 {
-	Load  = OpcodeDescriptor("load",  0x00, Encoding.A),
-	Store = OpcodeDescriptor("store", 0x01, Encoding.A),
-	Move  = OpcodeDescriptor("move",  0x02, Encoding.A),
-	AddA  = OpcodeDescriptor("add",   0x03, Encoding.A),
-	AddB  = OpcodeDescriptor("add",   0x04, Encoding.B),
-	Sub   = OpcodeDescriptor("sub",   0x05, Encoding.A),
-	Mul   = OpcodeDescriptor("mul",   0x06, Encoding.A),
-	Div   = OpcodeDescriptor("div",   0x07, Encoding.A),
-	Not   = OpcodeDescriptor("not",   0x08, Encoding.A),
-	And   = OpcodeDescriptor("and",   0x09, Encoding.A),
-	Or    = OpcodeDescriptor("or",    0x0A, Encoding.A),
-	Xor   = OpcodeDescriptor("xor",   0x0B, Encoding.A),
+	Load	= OpcodeDescriptor("load",		0x00, Encoding.A),
+	Store 	= OpcodeDescriptor("store",		0x01, Encoding.A),
+	LoadLi	= OpcodeDescriptor("loadli",	0x02, Encoding.B),
+	LoadUi	= OpcodeDescriptor("loadui",	0x03, Encoding.B),
+	Move	= OpcodeDescriptor("move",		0x04, Encoding.A),
+	AddA	= OpcodeDescriptor("add",		0x05, Encoding.A),
+	AddB	= OpcodeDescriptor("add",		0x06, Encoding.B),
+	Sub		= OpcodeDescriptor("sub",		0x07, Encoding.A),
+	Mul		= OpcodeDescriptor("mul",		0x08, Encoding.A),
+	Div		= OpcodeDescriptor("div",		0x09, Encoding.A),
+	Not		= OpcodeDescriptor("not",		0x0A, Encoding.A),
+	And		= OpcodeDescriptor("and",		0x0B, Encoding.A),
+	Or		= OpcodeDescriptor("or",		0x0C, Encoding.A),
+	Xor		= OpcodeDescriptor("xor",		0x0D, Encoding.A),
 }
 
 unittest
