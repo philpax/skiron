ebe339a50fc6f3f02fd7a34a7aa848dbb5d3ead7
Philpax <me@philpax.me>
2015-09-19 16:22:15 +1000

    Clean up assembler parsing code

--- assembler/main.d
+++ assembler/main.d
@@ -69,8 +69,13 @@ Token[] tokenise(string input)
 	return tokens;
 }
 
-ubyte toRegister(Token token)
+ubyte parseRegister(ref Token[] tokens)
 {
+	auto token = tokens.front;
+	enforce(token.type == Token.Type.Identifier);
+
+	scope (success) tokens.popFront();
+
 	auto t = token.text;
 	if (t == "ip")
 		return Register.IP;
@@ -82,86 +87,87 @@ ubyte toRegister(Token token)
 		return Register.Z;
 	else if (t.startsWith("r"))
 		return t[1..$].to!ubyte();
+	else
+		throw new Exception("Invalid register: " ~ t);
+}
 
-	assert(0);
+int parseNumber(ref Token[] tokens)
+{
+	auto token = tokens.front;
+	enforce(token.type == Token.Type.Number);
+
+	scope (success) tokens.popFront();
+	return token.number;
 }
 
 bool assembleDstSrc(ref Token[] tokens, ref const(OpcodeDescriptor) descriptor, ref uint[] output)
 {
-	if (tokens.length < 3)
-		return false;
-
-	auto operandTokens = tokens[1..3];
-	if (operandTokens.any!(a => a.type != Token.Type.Identifier))
-		return false;
+	auto newTokens = tokens;
 
 	Opcode opcode;
 	opcode.opcode = descriptor.opcode;
-	opcode.register1 = operandTokens[0].toRegister();
-	opcode.register2 = operandTokens[1].toRegister();
-	opcode.register3 = 0;
+	try
+	{
+		opcode.register1 = newTokens.parseRegister();
+		opcode.register2 = newTokens.parseRegister();
+		opcode.register3 = 0;
+	}
+	catch (Exception e)
+		return false;
 
 	output ~= opcode.value;
+	tokens = newTokens;
 
-	tokens = tokens[3..$];
 	return true;
 }
 
 bool assembleDstSrcSrc(ref Token[] tokens, ref const(OpcodeDescriptor) descriptor, ref uint[] output)
 {
-	if (tokens.length < 4)
-		return false;
-
-	auto operandTokens = tokens[1..4];
-	if (operandTokens.any!(a => a.type != Token.Type.Identifier))
-		return false;
+	auto newTokens = tokens;
 
 	Opcode opcode;
 	opcode.opcode = descriptor.opcode;
-	opcode.register1 = operandTokens[0].toRegister();
-	opcode.register2 = operandTokens[1].toRegister();
-	opcode.register3 = operandTokens[2].toRegister();
+	try
+	{
+		opcode.register1 = newTokens.parseRegister();
+		opcode.register2 = newTokens.parseRegister();
+		opcode.register3 = newTokens.parseRegister();
+	}
+	catch (Exception e)
+		return false;
 
 	output ~= opcode.value;
+	tokens = newTokens;
 
-	tokens = tokens[4..$];
 	return true;
 }
 
 bool assembleDstImm(ref Token[] tokens, ref const(OpcodeDescriptor) descriptor, ref uint[] output)
 {
-	if (tokens.length < 3)
-		return false;
-
-	auto operandTokens = tokens[1..3];
-	if (operandTokens[0].type != Token.Type.Identifier ||
-		operandTokens[1].type != Token.Type.Number)
-		return false;
+	auto newTokens = tokens;
 
 	Opcode opcode;
 	opcode.opcode = descriptor.opcode;
-	opcode.register1 = operandTokens[0].toRegister();
-	opcode.immediate = operandTokens[1].number;
+	try
+	{
+		opcode.register1 = newTokens.parseRegister();
+		opcode.immediate = newTokens.parseNumber();
+	}
+	catch (Exception e)
+		return false;
 
 	output ~= opcode.value;
+	tokens = newTokens;
 
-	tokens = tokens[3..$];
 	return true;
 }
 
 bool assembleNone(ref Token[] tokens, ref const(OpcodeDescriptor) descriptor, ref uint[] output)
 {
-	if (tokens.length < 1)
-		return false;
-
 	Opcode opcode;
 	opcode.opcode = descriptor.opcode;
-	opcode.register1 = 0;
-	opcode.immediate = 0;
-
 	output ~= opcode.value;
 
-	tokens = tokens[1..$];
 	return true;
 }
 
@@ -177,6 +183,8 @@ uint[] assemble(Token[] tokens)
 			auto descriptors = token.text in descriptors;
 			enforce(descriptors, "No matching opcode found for " ~ token.text);
 			bool foundMatching = false;
+
+			tokens.popFront();
 			foreach (descriptor; *descriptors)
 			{
 				if (descriptor.operandFormat == OperandFormat.DstSrc)
@@ -212,7 +220,7 @@ uint[] assemble(Token[] tokens)
 					}
 				}
 			}
-			enforce(foundMatching, "No valid opcodes found");
+			enforce(foundMatching, "No valid overloads for '" ~ token.text ~ "' found");
 		}
 		else
 		{
