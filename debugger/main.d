5deec520a7d677e1224de1faf812036e442eddd7
Philpax <me@philpax.me>
2016-04-02 01:04:44 +1100

    Implement basic debugger layout

--- debugger/main.d
+++ debugger/main.d
@@ -1,23 +1,102 @@
-import gio.Application : GioApplication = Application;
 import gtk.Application;
-import gtk.ApplicationWindow;
-import gtk.Label;
+
+// Windows
+import gtk.ApplicationWindow, gtk.Window;
+// Menu
+import gtk.MenuBar, gtk.MenuItem;
+// Input
+import gtk.Entry, gtk.Button;
+// Display
+import gtk.Label, gtk.TextView;
+// Layout
+import gtk.VBox, gtk.HBox;
+
+class ConnectWindow : Window
+{
+	Entry ipAddressEntry;
+	Entry portEntry;
+
+	Debugger debugger;
+
+	this(Debugger debugger)
+	{
+		super("Connect to Simulator");
+		
+		this.debugger = debugger;
+		this.setBorderWidth(10);
+
+		this.ipAddressEntry = new Entry("127.0.0.1");
+		this.portEntry = new Entry("1234");
+
+		auto vbox = new VBox(false, 2);
+
+		auto ipBox = new HBox(false, 2);
+		ipBox.packStart(new Label("IP address"), false, false, 5);
+		ipBox.packEnd(this.ipAddressEntry, false, false, 5);
+		vbox.packStart(ipBox, false, false, 5);
+
+		auto portBox = new HBox(false, 2);
+		portBox.packStart(new Label("Port"), false, false, 5);
+		portBox.packEnd(this.portEntry, false, false, 5);
+		vbox.packStart(portBox, false, false, 5);
+
+		vbox.packEnd(new Button("Connect", &this.onConnectClick), true, true, 5);
+		this.add(vbox);
+
+		this.setVisible(false);
+	}
+
+	void onConnectClick(Button)
+	{
+		this.setVisible(false);
+	}
+}
 
 class Debugger : ApplicationWindow
 {
+	MenuBar menu;
+	ConnectWindow connectWindow;
+	TextView logView;
+
 	this(Application application)
 	{
 		super(application);
-		setTitle("Skiron Debugger");
-		setBorderWidth(10);
+		this.setTitle("Skiron Debugger");
+		this.setDefaultSize(640, 480);
 
-		showAll();
+		auto vbox = new VBox(false, 0);
+
+		this.menu = new MenuBar();
+		this.menu.append(new MenuItem(&this.onConnectClick, "Connect"));
+		vbox.packStart(this.menu, false, false, 0);
+
+		this.logView = new TextView();
+		this.logView.setEditable(false);
+		vbox.packEnd(this.logView, true, true, 5);
+
+		this.add(vbox);
+
+		this.connectWindow = new ConnectWindow(this);
+
+		this.showAll();
+	}
+
+	void onConnectClick(MenuItem)
+	{
+		this.connectWindow.showAll();
+	}
+
+	void log(Args...)(string text, auto ref Args args)
+	{
+		import std.string;
+
+		this.logView.appendText((text ~ "\n").format(args));
 	}
 }
 
 int main(string[] args)
 {
 	auto application = new Application(null, GApplicationFlags.NON_UNIQUE);
-	application.addOnActivate(delegate void(GioApplication app) { new Debugger(application); });
+	application.addOnActivate((a) { new Debugger(application); });
 	return application.run(args);
 }
