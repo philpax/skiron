557a250213119c9316422eefaa0b2d97787c69c7
Philpax <me@philpax.me>
2016-05-01 01:25:13 +1000

    Move test running to its own function

--- test_runner/main.d
+++ test_runner/main.d
@@ -13,17 +13,61 @@ import common.cpu;
 
 import debugger_backend.backend;
 
+alias TargetType = Signed!RegisterType;
+struct Test
+{
+	string name;
+	string path;
+	TargetType[Register] targets;
+
+	void run()
+	{
+		bool running = true;
+		auto debugger = new Debugger();
+
+		debugger.onInitialize = {
+			debugger.cores[0].setRunning(true);
+		};
+
+		debugger.onDisconnect = {
+			running = false;
+		};
+
+		debugger.onLog = (string s) {
+			"%s | %s".writefln(this.name, s);
+		};
+
+		debugger.onCoreHalt = (Core* core) {
+			if (core.running) return;
+
+			bool success = true;
+
+			foreach (register, value; this.targets)
+			{
+				auto coreValue = cast(TargetType)core.registers[register];
+				
+				if (coreValue != value)
+				{
+					"%s | %s: Expected %s, got %s".writefln(this.name, register.registerName(), value, coreValue);
+					success = false;
+				}
+			}
+
+			"%s | %s".writefln(success ? "Pass" : "Fail", this.name);
+			debugger.shutdown();
+			debugger.waitForSpawnedEmulator();
+			running = false;
+		};
+
+		debugger.spawnEmulator(this.path, 1234, true);
+
+		while (running)
+			debugger.handleSocket();
+	}
+}
+
 void main()
 {
-	alias TargetType = Signed!RegisterType;
-
-	struct Test
-	{
-		string name;
-		string path;
-		TargetType[Register] targets;
-	}
-
 	Test[] tests;
 
 	foreach (filePath; "../tests".dirEntries("*.skasm", SpanMode.depth))
@@ -49,47 +93,5 @@ void main()
 	writeln("== Tests: ", tests.map!(a => a.name).join(", "), " ==");
 
 	foreach (test; tests)
-	{
-		bool run = true;
-		auto debugger = new Debugger();
-
-		debugger.onInitialize = {
-			debugger.cores[0].setRunning(true);
-		};
-
-		debugger.onDisconnect = {
-			run = false;
-		};
-
-		debugger.onLog = (string s) {
-			"%s | %s".writefln(test.name, s);
-		};
-
-		debugger.onCoreHalt = (Core* core) {
-			if (core.running) return;
-
-			bool success = true;
-
-			foreach (register, value; test.targets)
-			{
-				auto coreValue = cast(TargetType)core.registers[register];
-				
-				if (coreValue != value)
-				{
-					"%s | %s: Expected %s, got %s".writefln(test.name, register.registerName(), value, coreValue);
-					success = false;
-				}
-			}
-
-			"%s | %s".writefln(success ? "Pass" : "Fail", test.name);
-			debugger.shutdown();
-			debugger.waitForSpawnedEmulator();
-			run = false;
-		};
-
-		debugger.spawnEmulator(test.path, 1234, true);
-
-		while (run)
-			debugger.handleSocket();
-	}
+		test.run();
 }
