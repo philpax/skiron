55f3793f8cdc4deffb77ea18752f1facd70adac5
Philpax <me@philpax.me>
2016-04-30 15:24:15 +1000

    Put vendor projects into a group in VS solution

--- premake5.lua
+++ premake5.lua
@@ -1,143 +1,145 @@
 solution "skiron"
 	configurations { "release", "debug", "unittest" }
 
-	project "gtkd"
-		kind "StaticLib"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
+	group "vendor"
+		project "gtkd"
+			kind "StaticLib"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
 
-		files { "vendor/gtkd/src/**.d" }
+			files { "vendor/gtkd/src/**.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "arsd"
-		kind "StaticLib"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "arsd"
+			kind "StaticLib"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		files { "vendor/arsd/simpledisplay.d", "vendor/arsd/color.d" }
+			files { "vendor/arsd/simpledisplay.d", "vendor/arsd/color.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "common"
-		kind "StaticLib"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+	group ""
+		project "common"
+			kind "StaticLib"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		files { "common/**.d" }
+			files { "common/**.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }		
+			filter "configurations:unittest"
+				flags { "UnitTest" }		
 
-	project "emulator"
-		kind "ConsoleApp"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "emulator"
+			kind "ConsoleApp"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		links { "bin/common.lib", "bin/arsd.lib" }
-		includedirs { "common/", "vendor/" }
-		files { "emulator/**.d" }
+			links { "bin/common.lib", "bin/arsd.lib" }
+			includedirs { "common/", "vendor/" }
+			files { "emulator/**.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "assembler"
-		kind "ConsoleApp"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "assembler"
+			kind "ConsoleApp"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		links { "bin/common.lib" }
-		includedirs { "common/" }
-		files { "assembler/**.d" }
+			links { "bin/common.lib" }
+			includedirs { "common/" }
+			files { "assembler/**.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "disassembler"
-		kind "ConsoleApp"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "disassembler"
+			kind "ConsoleApp"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		links { "bin/common.lib" }
-		includedirs { "common/" }
-		files { "disassembler/**.d" }
+			links { "bin/common.lib" }
+			includedirs { "common/" }
+			files { "disassembler/**.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "debugger_backend"
-		kind "StaticLib"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "debugger_backend"
+			kind "StaticLib"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		links { "bin/common.lib" }
-		includedirs { "common/" }
-		files { "debugger_backend/**.d" }
+			links { "bin/common.lib" }
+			includedirs { "common/" }
+			files { "debugger_backend/**.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "debugger_graphical"
-		kind "WindowedApp"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "debugger_graphical"
+			kind "WindowedApp"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		includedirs { "vendor/gtkd/src", "common/", "debugger_backend/" }
-		files { "debugger_graphical/**.d" }
-		links { "bin/gtkd.lib", "bin/debugger_backend.lib", "bin/common.lib" }
+			includedirs { "vendor/gtkd/src", "common/", "debugger_backend/" }
+			files { "debugger_graphical/**.d" }
+			links { "bin/gtkd.lib", "bin/debugger_backend.lib", "bin/common.lib" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "test_runner"
-		kind "ConsoleApp"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "test_runner"
+			kind "ConsoleApp"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		links { "bin/debugger_backend.lib", "bin/common.lib" }
-		includedirs { "common/", "debugger_backend/" }
-		files { "test_runner/**.d" }
+			links { "bin/debugger_backend.lib", "bin/common.lib" }
+			includedirs { "common/", "debugger_backend/" }
+			files { "test_runner/**.d" }
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
 
-	project "docgen"
-		kind "ConsoleApp"
-		language "D"
-		targetdir "bin"
-		debugdir "bin"
-		flags { "SymbolsLikeC" }
+		project "docgen"
+			kind "ConsoleApp"
+			language "D"
+			targetdir "bin"
+			debugdir "bin"
+			flags { "SymbolsLikeC" }
 
-		links { "bin/common.lib" }
-		includedirs { "common/" }
-		files { "docgen/**.d" }
+			links { "bin/common.lib" }
+			includedirs { "common/" }
+			files { "docgen/**.d" }
 
-		filter "action:vs*"
-			postbuildcommands "chdir bin && docgen && chdir ../"
+			filter "action:vs*"
+				postbuildcommands "chdir bin && docgen && chdir ../"
 
-		filter "action:not vs*"
-			postbuildcommands "{CHDIR} bin && ./docgen"
+			filter "action:not vs*"
+				postbuildcommands "{CHDIR} bin && ./docgen"
 
-		filter {}
+			filter {}
 
-		filter "configurations:unittest"
-			flags { "UnitTest" }
+			filter "configurations:unittest"
+				flags { "UnitTest" }
