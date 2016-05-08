4a78b1ca005bde3a3b486f13ceee135b59663ea1
Philpax <me@philpax.me>
2016-05-09 00:55:58 +1000

    Supply string handlers for all DstSrcSrc opcodes

--- emulator/instruction/arithmetic.d
+++ emulator/instruction/arithmetic.d
@@ -15,47 +15,7 @@ void runAddD(Type = uint)(ref Core core, Opcode opcode)
 	core.setDst!Type(opcode, core.getSrc1!Type(opcode) + core.getImmediate(opcode));
 }
 
-void runSub(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) - core.getSrc2!Type(opcode));
-}
-
-void runMul(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) * core.getSrc2!Type(opcode));
-}
-
-void runDiv(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) / core.getSrc2!Type(opcode));
-}
-
 void runNot(Type = uint)(ref Core core, Opcode opcode)
 {
 	core.setDst!Type(opcode, ~core.getSrc!Type(opcode));
-}
-
-void runAnd(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) & core.getSrc2!Type(opcode));
-}
-
-void runOr(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) | core.getSrc2!Type(opcode));
-}
-
-void runXor(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) ^ core.getSrc2!Type(opcode));
-}
-
-void runShl(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) << core.getSrc2!Type(opcode));
-}
-
-void runShr(Type = uint)(ref Core core, Opcode opcode)
-{
-	core.setDst!Type(opcode, core.getSrc1!Type(opcode) >> core.getSrc2!Type(opcode));
 }
